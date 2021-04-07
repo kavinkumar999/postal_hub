cc475a50db0b25d1e2d56c3e654d4a69d7d2e924
kavin <kavinkumarnkm007@gmail.com>
2021-04-07 22:34:11 +0530

    feat[__init__]: update the hook function

--- postal_hub/__init__.py
+++ postal_hub/__init__.py
@@ -2,22 +2,18 @@
 from __future__ import unicode_literals
 from postal_hub.postal_hub.doctype import facebook , twitter, insta 
 import frappe
+from postal_hub.postal_hub.doctype.postal_post.postal_post import post
 __version__ = '0.0.1'
 
 @frappe.whitelist()
 def scheduledpost():
-<<<<<<< HEAD
+	# query
+	posted = frappe.db.get("select post_image, caption, is_complete ,post_time ,name from `tabPostal Post` where is_complete = 0",as_dict = 1)
+	for _post in posted:
+		if _post["post_time"] < frappe.utils.now():
+			post(_post["name"])
 	print("scheduledpost")
 
 @frappe.whitelist()
 def posting():
-	print("posting")
-=======
-	print("hellp")
-	print("//////////////////////////////////////////")
-
-@frappe.whitelist()
-def posting():
-	print("hellp")
-	print("//////////////////////////////////////////")
->>>>>>> 20a912be687fba594ca2176d44a367d73581f8dd
+	return "posted"
---
+++ postal_hub/postal_hub/doctype/postal_post/__init__.py
@@ -0,0 +1,3 @@
+url = "https://app.ayrshare.com/api/post"
+hashtag = "#facebookshare #twittershare #mediashare #post #fuseify"
+token = "VBE74YA-M584XFR-JQ67S48-7G4PR0R"
